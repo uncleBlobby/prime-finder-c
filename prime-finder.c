dbac9f4e510a1bc38b88e1e5dc0f3b7abdb36629
Dustin Christianson <dc.christianson@gmail.com>
2022-05-11 13:33:40 -0600

    added basic timer function and logic to print total primes found every 1000 primes

--- prime-finder.c
+++ prime-finder.c
@@ -1,29 +1,40 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <time.h>
 
 int main(){
-    printf("Hello, C\n");
+    time_t start = time(0);
+    printf("Prime-Finder\n");
+    printf("Start Time: %d\n", start);
 
+    int count = 0;
     int k = 0;
-
-    for (int i = 0; i < 10; i++){
-
-        printf("%d\n", i);
-    }
 
     while(1){
         k++;
         bool isPrime = true;
         for (int i = 2; i < k; i++){
             if (k % i == 0){
-                printf("%d is not a prime.\n", k);
+                //printf("%d is not a prime.\n", k);
                 isPrime = false;
                 break;
             }
         }
         if (isPrime) {
-            printf("%d is a prime.\n", k);
+            //printf("%d is a prime.\n", k);
+            count++;
+            if (count % 1000 == 0){
+               time_t current = time(0);
+               int duration = current - start;
+               printf("%d primes found, in %d seconds.\n", count, duration);
+               if (duration > 0){
+                   int average = count / duration;
+                   printf("Average: %d primes per second.\n", average);   
+               }
+               
+            }
         }
+
     }
     
     return 0;
